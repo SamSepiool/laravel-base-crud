2c507f1208e1526db219312a213ac9266d552049
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-11-11 23:08:15 +0100

    add delete method

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,37 +4,50 @@
 
 @section('content')
 
-<table class="table">
-    <thead>
-      <tr>
-            <th scope="col">id</th>
-            <th scope="col">Titolo</th>
-            <th scope="col">Prezzo</th>
-            <th scope="col">Serie</th>
-            <th scope="col">Data d'uscita</th>
-            <th scope="col">Tipologia</th>
-            <th scope="col">Azioni</th>
-      </tr>
-    </thead>
-    <tbody>
+<div class="container">
+    <table class="table">
+        <thead>
+          <tr>
+                <th scope="col">id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data d'uscita</th>
+                <th scope="col">Tipologia</th>
+                <th scope="col">|</th>
+                <th scope="col" class="px-0">Azioni</th>
+                <th scope="col" class="text-right">|</th>
+
+          </tr>
+        </thead>
+        <tbody>
+            
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{$comic['id']}}</th>
+                    <td>{{$comic['title']}}</td>
+                    <td>{{$comic['price']}}</td>
+                    <td>{{$comic['series']}}</td>
+                    <td>{{$comic['sale_date']}}</td>
+                    <td>{{$comic['type']}}</td>
+                    <td>
+                        <a href="{{route('comics.show', $comic['id'])}}"><button type="button" class="btn btn-primary">Visualizza</button></a>
+                    </td>
+                    <td>
+                        <a href="{{route('comics.edit', $comic['id'])}}"><button type="button" class="btn btn-warning">Modifica</button></a>                    
+                    </td>
+                    <td>
+                        <form action="{{route('comics.destroy', $comic['id'])}}" method="POST">
+                            @csrf
+                            {{-- DELETE METHOD --}}
+                            @method('DELETE')
+                            <button type="SUBMIT" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                        </form>
+                    </td>
+                </tr> 
+            @endforeach
         
-        @foreach ($comics as $comic)
-            <tr>
-                <th scope="row">{{$comic['id']}}</th>
-                <td>{{$comic['title']}}</td>
-                <td>{{$comic['price']}}</td>
-                <td>{{$comic['series']}}</td>
-                <td>{{$comic['sale_date']}}</td>
-                <td>{{$comic['type']}}</td>
-                <td>
-                    <a href="{{route('comics.show', $comic['id'])}}"><button type="button" class="btn btn-primary">Visualizza</button></a>
-                </td>
-                <td>
-                    <a href="{{route('comics.edit', $comic['id'])}}"><button type="button" class="btn btn-warning">Modifica</button></a>                    
-                </td>
-            </tr> 
-        @endforeach
-    
-    </tbody>
-  </table>
+        </tbody>
+    </table>
+</div>
 @endsection
